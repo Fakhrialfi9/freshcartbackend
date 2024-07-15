82910907bc46303b5ba77c90868e9f0a1c2dcd97
Fakhrialfi9 <Fakhrialfi9@gmail.com>
2024-07-15 23:46:46 +0700

    Prepare for deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// Import necessary modules
+// app.js
+
 import express from "express";
-import session from "express-session";
+import session from "express-session"; // Import session module
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -9,76 +10,56 @@ import morgan from "morgan";
 import router from "./routes/routes.js";
 import mongoSanitize from "express-mongo-sanitize";
 import helmet from "helmet";
+
 import { errorHandler, notFound } from "./middleware/errorhandler.js";
 import { sendOTPByPhoneNumber, verifyOTPByPhoneNumber } from "./controllers/OTPByPhoneNumberControllers.js";
 import { sendOTPByEmail, verifyOTPByEmail } from "./controllers/OTPByEmailControllers.js";
 
-// Load environment variables
 dotenv.config();
 
-// Initialize Express app
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Security middleware
-app.use(helmet()); // Set various HTTP headers for security
-app.use(mongoSanitize()); // Prevent MongoDB Operator Injection
-app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+// Middleware
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-// Session configuration
+// Configure session
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: {
-      httpOnly: true, // Prevent client-side JavaScript access to cookies
-      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-      sameSite: "strict", // Mitigate CSRF attacks by asserting same-site
-    },
+    cookie: { secure: false },
   }),
 );
 
-// Middleware for parsing application/json
-app.use(express.json());
-
-// Middleware for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware for cookie parsing
-app.use(cookieParser());
-
-// Logging middleware (use only in development environment)
-if (process.env.NODE_ENV === "production") {
-  app.use(morgan("dev"));
-}
-
-// Set X-Frame-Options header to mitigate clickjacking
-app.use((req, res, next) => {
-  res.setHeader("X-Frame-Options", "SAMEORIGIN");
-  next();
-});
-
-// Route for root endpoint
+// Endpoint for root
 app.get("/api/v1", (req, res) => {
-  res.send("API is working");
+  res.send("Test API is working");
 });
 
-// Routes for OTP Phone Number
+// Use the OTP Phone Number router
 app.use("/api/v1/auth/phone", sendOTPByPhoneNumber, verifyOTPByPhoneNumber);
 
-// Route for OTP Email
+// Use the OTP Email router
 app.use("/api/v1/auth/email", sendOTPByEmail);
 
-// Route for verifying OTP Email
+// Use the OTP Email router
 app.use("/api/v1/auth/verify", verifyOTPByEmail);
 
-// Use router defined in routes.js
+// Use the router defined in routes.js
 app.use("/api/v1/auth", router);
 
-// Middleware for error handling
-app.use(notFound); // Handle 404 errors
-app.use(errorHandler); // Centralized error handling
+app.use(errorHandler);
+app.use(notFound);
 
 // Connect to MongoDB
 mongoose
@@ -92,5 +73,5 @@ mongoose
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server berjalan pada http://localhost:${port}`);
 });
